12949a4908e57f0c967824e6244ab6a6fd27edbd
CreativeMetalCat <catguy228@gmail.com>
2020-12-25 19:04:50 +1000

    added stuff

--- SFML/Source.cpp
+++ SFML/Source.cpp
@@ -1,16 +1,27 @@
 #include <SFML/Main.hpp>
 #include <SFML/Graphics.hpp>
+
+#include "TestClasses/TextClassShape.hpp"
+
 #include <iostream>
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(200, 200),"It works!");
-
+	Test::TestShape* shape = new Test::TestShape(200, sf::Color::Red);
 	while (window.isOpen())
 	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 		window.clear();
+		shape->Draw(window);
 		window.display();
 	}
+
 
 	return 0;
 }
--- SFML/TestClasses/TextClassShape.hpp
+++ SFML/TestClasses/TextClassShape.hpp
@@ -1,17 +1,30 @@
 #include <SFML/Graphics.hpp>
 
-class TestShape 
+namespace Test 
 {
-public:
-	sf::CircleShape shape;
-
-	float CircleRaius = 200;
-
-	sf::Color color;
-
-	TestShape(float _CircleRaius, sf::Color _color) :CircleRaius(_CircleRaius)
+	class TestShape
 	{
-		shape.setRadius(CircleRaius);
+	public:
+		sf::CircleShape shape;
 
-	}
-};
+		float CircleRaius = 200;
+
+		sf::Color color;
+
+		TestShape(float _CircleRaius, sf::Color _color) :CircleRaius(_CircleRaius), color(_color)
+		{
+			shape.setRadius(CircleRaius);
+			shape.setFillColor(_color);
+		}
+
+		virtual void Draw(sf::RenderWindow &window)
+		{
+			window.draw(shape);
+		}
+
+		virtual void Destroy()
+		{
+			//clean memory here
+		}
+	};
+}
